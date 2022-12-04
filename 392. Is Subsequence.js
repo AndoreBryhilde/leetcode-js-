d2ebd21a3fb8dd3e5de048bbdca9689fb0dc827d
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-04 19:23:07 +0800

    Create 392. Is Subsequence.js
    392. Is Subsequence

---
+++ 392. Is Subsequence.js
@@ -0,0 +1,37 @@
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters 
+without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+Example 1:
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+Constraints:
+0 <= s.length <= 100
+0 <= t.length <= 104
+s and t consist only of lowercase English letters.
+ 
+
+Follow up: Suppose there are lots of incoming s, say s1, s2, ..., sk where k >= 109, 
+  and you want to check one by one to see if t has its subsequence. In this scenario, how would you change your code?
+    
+    /**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    let i=0;
+    let sl=s.length;
+    let tl=t.length;
+    for(let j=0;j<tl&&i<sl;j++){
+        if(s[i]==t[j])i++;
+    }
+    return i==sl;
+};
