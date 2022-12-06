74c4ad18f516ae7e52bc2ce17b1137b990cdcec3
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-06 20:46:42 +0800

    Create 2. Add Two Numbers.js
    2. Add Two Numbers

---
+++ 2. Add Two Numbers.js
@@ -0,0 +1,47 @@
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+Constraints:
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let head=new ListNode();
+    let node=head;
+    let i=0;
+    while(l1||l2||i){
+        let s=i;
+        if(l1){s+=l1.val;l1=l1.next;}
+        if(l2){s+=l2.val;l2=l2.next;}
+        i=s>9?1:0;
+        node.next=new ListNode(s%10);
+        node=node.next;
+    }
+    return head.next;
+};
