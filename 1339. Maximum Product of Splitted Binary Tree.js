ff8fb56045ef6e0b60187a08259ec81d075d5b01
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-10 17:37:37 +0800

    Create 1339. Maximum Product of Splitted Binary Tree.js
    1339. Maximum Product of Splitted Binary Tree

---
+++ 1339. Maximum Product of Splitted Binary Tree.js
@@ -0,0 +1,49 @@
+Given the root of a binary tree, split the binary tree into two subtrees by removing one edge such that the product of the sums of the subtrees is maximized.
+Return the maximum product of the sums of the two subtrees. Since the answer may be too large, return it modulo 109 + 7.
+Note that you need to maximize the answer before taking the mod and not after taking it.
+
+Example 1:
+Input: root = [1,2,3,4,5,6]
+Output: 110
+Explanation: Remove the red edge and get 2 binary trees with sum 11 and 10. Their product is 110 (11*10)
+
+Example 2:
+Input: root = [1,null,2,3,4,null,null,5,6]
+Output: 90
+Explanation: Remove the red edge and get 2 binary trees with sum 15 and 6.Their product is 90 (15*6)
+
+Constraints:
+The number of nodes in the tree is in the range [2, 5 * 104].
+1 <= Node.val <= 104
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxProduct = function(root) {
+   let ans=0;
+   let sum=0;
+   function track(node){
+       if(!node)return 0;
+       sum+=node.val;
+       track(node.left);
+       track(node.right);
+   }
+   track(root);
+   function cal(node){
+       if (!node)return 0;
+        let s = cal(node.left) + cal(node.right) + node.val;
+        ans = Math.max(ans,s*(sum-s));
+        return s;
+   }
+   cal(root)
+   return ans%(Math.pow(10,9)+7);
+};
