639c854449fdffbb27d736c3a4197496bfaa0306
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-14 00:37:42 +0800

    Create 504. Base 7.js
    504. Base 7

---
+++ 504. Base 7.js
@@ -0,0 +1,20 @@
+Given an integer num, return a string of its base 7 representation.
+
+Example 1:
+Input: num = 100
+Output: "202"
+
+Example 2:
+Input: num = -7
+Output: "-10"
+
+Constraints:
+-107 <= num <= 107
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    return num.toString(7);
+};
