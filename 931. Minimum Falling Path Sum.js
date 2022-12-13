d734387d023a6b69d655c9bd8be9027e9a28f07e
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-14 00:39:11 +0800

    Create 931. Minimum Falling Path Sum.js
    931. Minimum Falling Path Sum

---
+++ 931. Minimum Falling Path Sum.js
@@ -0,0 +1,34 @@
+Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+
+Example 1:
+Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+Output: 13
+Explanation: There are two falling paths with a minimum sum as shown.
+
+Example 2:
+Input: matrix = [[-19,57],[-40,-5]]
+Output: -59
+Explanation: The falling path with a minimum sum is shown.
+
+Constraints:
+n == matrix.length == matrix[i].length
+1 <= n <= 100
+-100 <= matrix[i][j] <= 100
+
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var minFallingPathSum = function(matrix) {
+    let l=matrix.length;
+    let a=[...matrix[0]];
+    for(let i=1;i<l;i++){
+        let t=[...a];
+        for(let j=0;j<l;j++){
+            a[j]=
+            Math.min(t[j-1]||Infinity,t[j],t[j+1]||Infinity)+matrix[i][j]
+        }
+    }
+    return Math.min(...a);
+};
