634a53e017e15ed56b5aadd4e4b00215ca5b0c5b
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-21 00:26:31 +0800

    Create 572. Subtree of Another Tree.js
    572. Subtree of Another Tree

---
+++ 572. Subtree of Another Tree.js
@@ -0,0 +1,41 @@
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+ 
+Constraints:
+The number of nodes in the root tree is in the range [1, 2000].
+The number of nodes in the subRoot tree is in the range [1, 1000].
+-104 <= root.val <= 104
+-104 <= subRoot.val <= 104
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if(!root)return !subRoot;
+    if(root.val==subRoot.val&&check(root,subRoot))return true;
+    return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot)
+};
+
+function check(n1,n2){
+        if(!n1||!n2)return !n1&&!n2;
+        if(n1.val!=n2.val)return false;
+        return check(n1.left,n2.left)&&check(n1.right,n2.right);
+}
