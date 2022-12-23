e5c24af62961c2defcf56d49d6aa2c27c86146c4
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2022-12-23 21:02:49 +0800

    Create 653. Two Sum IV - Input is a BST.js
    653. Two Sum IV - Input is a BST

---
+++ 653. Two Sum IV - Input is a BST.js
@@ -0,0 +1,43 @@
+Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+
+Example 1:
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+
+Example 2:
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    let ans=[];
+    function track(node){
+        if(!node)return ;
+        ans.push(node.val);
+        track(node.left);
+        track(node.right);
+    }
+    track(root);
+    for(let i=0;i<ans.length;i++){
+        if(~ans.indexOf(k-ans[i],i+1))return true;
+    }
+    return false;
+};
