775636721caccbd760b357f2fb4529c3f642a577
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2023-01-01 00:15:34 +0800

    Update 705. Design HashSet.js

--- 705. Design HashSet.js
+++ 705. Design HashSet.js
@@ -60,3 +60,41 @@ MyHashSet.prototype.contains = function(key) {
  * obj.remove(key)
  * var param_3 = obj.contains(key)
  */
+
+
+//一個非常巧妙利用js皆為物件的技巧
+var MyHashSet = function() {
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function(key) {
+    if(key in this) return
+    this[key] = null
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function(key) {
+    delete this[key]
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function(key) {
+    return key in this
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
