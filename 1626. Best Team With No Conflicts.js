a34943b91052fdd61918b2d0d97af6a9ced05d4a
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2023-02-01 00:13:52 +0800

    Update 1626. Best Team With No Conflicts.js

--- 1626. Best Team With No Conflicts.js
+++ 1626. Best Team With No Conflicts.js
@@ -56,3 +56,30 @@ var bestTeamScore = function(scores, ages) {
     }
     return max;
 };
+
+//better way
+var bestTeamScore = function(scores, ages) {
+    const len = ages.length;
+    const players = []
+
+    const dp = Array.from({ length: len}, () => 0)
+    for (let i=0; i<len; i++) {
+        players.push({
+            score: scores[i],
+            age: ages[i]
+        })
+    }
+
+    players.sort((a, b) => a.age === b.age ? b.score - a.score : b.age - a.age);
+
+    for(let i=0; i<len; i++) {
+        dp[i] = players[i].score
+        for(let j=0; j<i; j++) {
+            if (players[j].score >= players[i].score) {
+                dp[i] = Math.max(dp[i], dp[j] + players[i].score)
+            }
+        }
+    }
+
+    return Math.max(...dp)
+};
