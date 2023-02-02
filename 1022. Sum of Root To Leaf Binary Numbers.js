cd8cb9cdc84d7627f7d62546eb26746bd3e5dfb7
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2023-02-02 23:04:50 +0800

    Create 1022. Sum of Root To Leaf Binary Numbers.js
    1022. Sum of Root To Leaf Binary Numbers.js

---
+++ 1022. Sum of Root To Leaf Binary Numbers.js
@@ -0,0 +1,41 @@
+You are given the root of a binary tree where each node has a value 0 or 1. Each root-to-leaf path represents a binary number starting with the most significant bit.
+For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf. Return the sum of these numbers.
+The test cases are generated so that the answer fits in a 32-bits integer.
+
+Example 1:
+Input: root = [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+Example 2:
+Input: root = [0]
+Output: 0
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+Node.val is 0 or 1.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumRootToLeaf = function(root) {
+    let sum=0;
+    function track(node,total){
+        if(!node)return;
+        total=(total<<1)|node.val;
+        if(!node.left&&!node.right){return sum+=total;}
+        else{track(node.left,total);track(node.right,total);}   
+    }
+    track(root,0);
+    return sum;
+};
