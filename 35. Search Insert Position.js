a284cc4f79de69a371a10f6616bfaa654ca2b118
AndoreBryhilde <115485922+AndoreBryhilde@users.noreply.github.com>
2023-02-20 14:35:23 +0800

    Create 35. Search Insert Position.js
    35. Search Insert Position

---
+++ 35. Search Insert Position.js
@@ -0,0 +1,43 @@
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+Constraints:
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+//javascript
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>=target){return i;}
+    }
+    return nums.length;
+};
+
+//go lang
+func searchInsert(nums []int, target int) int {
+    for i:=0;i<len(nums);i++ {
+        if nums[i]>=target {
+            return i;
+        }
+    }
+    return len(nums);
+}
